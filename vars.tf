087ecd42a623d504c337f363e2ed5a36be3f1e9d
Andrei Chenchik <andrei@chenchik.me>
2019-12-06 22:29:49 +0300

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,25 @@
+# external variables 
+variable "module_count" {}
+variable "node_pool" {}
+variable "persistent_disk" {}
+
+#internal variables
+variable "app_name" {
+  default="ddns"
+}
+
+variable "container_name" {
+  default="ddns-container"
+}
+
+variable "deployment_name" {
+  default="ddns-deployment"
+}
+
+variable "image" {
+  default="gumlooter/libcloud-dynamic-dns:latest"
+}
+  
+variable "persistent_mount_path" {
+  default="/home/jovyan/work"
+}
